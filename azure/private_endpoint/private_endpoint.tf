7e3a698711393c7db2295a7ca23240d4a0171ab4
DeVante Pickering <devante@pick2solutions.com>
2025-06-06 22:13:14 -0400

    add module for storage account w/ backup.

--- azure/private_endpoint/private_endpoint.tf
+++ azure/private_endpoint/private_endpoint.tf
@@ -1,9 +1,9 @@
 resource "azurerm_private_endpoint" "this" {
   for_each            = { for idx, sub in var.subresources : "${sub.name}" => sub }
-  name               = "pe-${each.key}-${var.name_suffix}"
-  location           = var.location
+  name                = "pe-${each.key}-${var.name_suffix}"
+  location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id          = var.subnet_id
+  subnet_id           = var.subnet_id
 
   private_service_connection {
     name                           = "psc-${each.key}-${var.name_suffix}"
@@ -21,10 +21,10 @@ resource "azurerm_private_endpoint" "this" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "this" {
-  for_each            = var.enable_vnet_link ? var.dns_zone_ids : {}
-  name                = "vnet-link-${each.key}-${var.name_suffix}"
-  resource_group_name = var.resource_group_name
+  for_each              = var.enable_vnet_link ? var.dns_zone_ids : {}
+  name                  = "vnet-link-${each.key}-${var.name_suffix}"
+  resource_group_name   = var.resource_group_name
   private_dns_zone_name = element(split("/", each.value), length(split("/", each.value)) - 1)
-  virtual_network_id  = var.vnet_id
-  registration_enabled = false
+  virtual_network_id    = var.vnet_id
+  registration_enabled  = false
 }
